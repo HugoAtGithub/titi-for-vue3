1e61117bd7462f0f03832de01c87dcf91350f575
HugoAtGithub <jorcion@gmail.com>
2020-11-11 00:29:44 +0800

    完善 aside 切换逻辑

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="topnav">
-    <div class="logo" @click="toggleMenu">LOGO</div>
+    <div class="logo">LOGO</div>
     <ul class="menu">
-      <li>菜单0</li>
       <li>菜单1</li>
+      <li>菜单2</li>
     </ul>
+    <span class="toggleAside" @click="toggleMenu"></span>
   </div>
 </template>
 
@@ -25,12 +26,13 @@ export default {
 <style lang="scss" scoped>
 
 .topnav {
-  border: 1px solid red;
   background: pink;
   display: flex;
   padding: 16px;
   position: relative;
   z-index: 10;
+  justify-content: center;
+  align-items: center;
 
   > .logo {
     max-width: 6em;
@@ -44,6 +46,29 @@ export default {
 
     > li {
       margin: 0 1em;
+    }
+  }
+
+  > .toggleAside {
+    display: none;
+    width: 24px;
+    height: 24px;
+    background: red;
+    position: absolute;
+    left: 16px;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+
+  @media (max-width: 500px) {
+    > .menu {
+      display: none;
+    }
+    > .logo {
+      margin: 0 auto;
+    }
+    > .toggleAside {
+      display: inline-block;
     }
   }
 }
