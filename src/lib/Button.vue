72d92aae4b2414ceb4ec5cfdfd1d7b31071857fc
HugoAtGithub <jorcion@gmail.com>
2020-11-12 00:09:09 +0800

    创建 button 组件

---
+++ src/lib/Button.vue
@@ -0,0 +1,23 @@
+<template>
+  <div :size="size">
+    <button v-bind="rest">
+      <slot/>
+    </button>
+  </div>
+</template>
+<script lang="ts">
+export default {
+  inheritAttrs: false,
+  props: {},
+  setup(props, context) {
+    const {size, ...rest} = context.attrs;
+    console.log(size, rest)
+    return {size, rest}
+  }
+}
+</script>
+<style lang="scss">
+div {
+  border: 1px solid red;
+}
+</style>
