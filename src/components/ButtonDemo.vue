5f354c403e937d6bcdc4c0ff234b6e861032dfe9
HugoAtGithub <jorcion@gmail.com>
2020-11-12 11:46:36 +0800

    支持 size 属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,10 +1,19 @@
 <template>
   <div> Button 示例</div>
-  <h1>示例1</h1>
-  <Button>你好</Button>
-  <Button theme="button">你好</Button>
-  <Button theme="link">你好</Button>
-  <Button theme="text">你好</Button>
+  <h1>theme 示例</h1>
+  <div>
+    <Button>你好</Button>
+    <Button theme="button">你好</Button>
+    <Button theme="link">你好</Button>
+    <Button theme="text">你好</Button>
+  </div>
+  <h1>size 示例</h1>
+  <div>
+    <Button size="big">大大大</Button>
+    <Button>普普通通</Button>
+    <Button size="small">小小小</Button>
+  </div>
+
 </template>
 <script lang="ts">
 import Button from '../lib/Button.vue'
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,77 +1,104 @@
 <template>
-  <button class="titi-button" :class="{[`titi-theme-${theme}`]: theme}">
+  <button class="titi-button" :class="classes">
     <slot/>
   </button>
 </template>
 <script lang="ts">
+import {computed} from 'vue'
+
 export default {
   props: {
     theme: {
       type: String,
       default: 'button'
+    },
+    size: {
+      type: String,
+      default: 'normal'
     }
   },
-
+  setup(props) {
+    const {theme, size} = props;
+    const classes = computed(() => {
+      return {
+        [`titi-theme-${theme}`]: theme,
+        [`titi-size-${size}`]: size,
+      }
+    })
+    return {classes}
+  }
 }
 </script>
 <style lang="scss">
-div {
-  $h: 32px;
-  $border-color: #d9d9d9;
-  $color: #333;
-  $blue: #40a9ff;
-  $radius: 4px;
+$h: 32px;
+$border-color: #d9d9d9;
+$color: #333;
+$blue: #40a9ff;
+$radius: 4px;
 
-  .titi-button {
-    box-sizing: border-box;
-    height: $h;
-    padding: 0 12px;
-    cursor: pointer;
-    display: inline-flex;
-    justify-content: center;
-    align-items: center;
-    white-space: nowrap;
-    background: white;
-    color: $color;
-    border: 1px solid $border-color;
-    border-radius: $radius;
-    box-shadow: 0 1px 0 fade-out(black, 0.95);
+.titi-button {
+  box-sizing: border-box;
+  height: $h;
+  padding: 0 12px;
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+  background: white;
+  color: $color;
+  border: 1px solid $border-color;
+  border-radius: $radius;
+  box-shadow: 0 1px 0 fade-out(black, 0.95);
 
-    & + & {
-      margin-left: 8px;
-    }
+  & + & {
+    margin-left: 8px;
+  }
+
+  &:hover, &:focus {
+    color: $blue;
+    border-color: $blue;
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  &::-moz-focus-inner {
+    border: none;
+  }
+
+  &.titi-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
 
     &:hover, &:focus {
-      color: $blue;
-      border-color: $blue;
+      color: lighten($blue, 10%);
+    }
+  }
+
+  &.titi-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+
+    &:hover, &:focus {
+      background: darken(white, 5%);
+    }
+  }
+
+  &.titi-theme-button {
+    &.titi-size-big {
+      font-size: 24px;
+      height: 48px;
+      padding: 0 16px;
     }
 
-    &:focus {
-      outline: none;
-    }
-
-    &::-moz-focus-inner {
-      border: none;
-    }
-
-    &.titi-theme-link {
-      border-color: transparent;
-      box-shadow: none;
-      color: $blue;
-
-      &:hover, &:focus {
-        color: lighten($blue, 10%);
-      }
-    }
-
-    &.titi-theme-text {
-      border-color: transparent;
-      box-shadow: none;
-      color: inherit;
-
-      &:hover, &:focus {
-        background: darken(white, 5%);
-      }
+    &.titi-size-small {
+      font-size: 12px;
+      height: 20px;
+      padding: 0 4px;
     }
   }
 }
