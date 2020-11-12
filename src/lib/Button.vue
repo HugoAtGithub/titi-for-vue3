ea33c484d4912f585210c9252158cd560dfb0f1f
HugoAtGithub <jorcion@gmail.com>
2020-11-12 12:27:19 +0800

    支持 level 属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -15,14 +15,19 @@ export default {
     size: {
       type: String,
       default: 'normal'
+    },
+    level: {
+      type: String,
+      default: 'normal'
     }
   },
   setup(props) {
-    const {theme, size} = props;
+    const {theme, size, level} = props;
     const classes = computed(() => {
       return {
         [`titi-theme-${theme}`]: theme,
         [`titi-size-${size}`]: size,
+        [`titi-level-${level}`]: level,
       }
     })
     return {classes}
@@ -35,6 +40,7 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
 
 .titi-button {
   box-sizing: border-box;
@@ -50,6 +56,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
 
   & + & {
     margin-left: 8px;
@@ -98,6 +105,58 @@ $radius: 4px;
     font-size: 12px;
     height: 20px;
     padding: 0 4px;
+  }
+
+  &.titi-theme-button {
+    &.titi-level-main {
+      background: $blue;
+      border-color: $blue;
+      color: white;
+
+      &:hover, &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+
+    &.titi-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+
+      &:hover, &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.titi-theme-link {
+    &.titi-level-danger {
+      color: red;
+
+      &:hover, &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.titi-theme-text {
+    &.titi-level-main {
+      color: $blue;
+
+      &:hover, &:focus {
+        color: darken($blue, 10%)
+      }
+    }
+
+    &.titi-level-danger {
+      color: $red;
+
+      &:hover, &:focus {
+        color: darken($red, 10%)
+      }
+    }
   }
 }
 </style>
