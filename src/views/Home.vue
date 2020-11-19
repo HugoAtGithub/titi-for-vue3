1af6df8ff4c36a8149961297f5df84a4fdf2c95c
HugoAtGithub <jorcion@gmail.com>
2020-11-19 16:20:32 +0800

    从 iconfont.cn 引入图标

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,27 @@
 <template>
-  <div class="topnavAndBanner">
-    <Topnav/>
-    <div class="banner">
-      <h1>titi UI</h1>
-      <h2>造个小轮子</h2>
-      <p class="actions">
-        <a href="https://github.com">Github</a>
-        <router-link to="/doc">开始</router-link>
-      </p>
+  <div>
+    <div class="topnavAndBanner">
+      <Topnav/>
+      <div class="banner">
+        <h1>titi UI</h1>
+        <h2>常用 UI 组件</h2>
+        <p class="actions">
+          <a href="https://github.com">Github</a>
+          <router-link to="/doc">开始</router-link>
+        </p>
+      </div>
     </div>
+  </div>
+  <div class="features">
+    <svg class="icon">
+      <use xlink:href="#icon-vue"></use>
+    </svg>
+    <svg class="icon">
+      <use xlink:href="#icon-ts"></use>
+    </svg>
+    <svg class="icon">
+      <use xlink:href="#icon-light"></use>
+    </svg>
   </div>
 </template>
 
@@ -22,11 +35,23 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$green: #02bcb0;
+$border-radius: 4px;
+$color: #007974;
+
 .topnavAndBanner {
   background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
 }
 
+.features {
+  > svg {
+    width: 64px;
+    height: 64px;
+  }
+}
+
 .banner {
+  color: $color;
   padding: 100px 0;
   display: flex;
   justify-content: center;
@@ -37,14 +62,16 @@ export default {
     padding: 8px 0;
 
     a {
-      background: white;
+      background: $green;
+      color: white;
       display: inline-block;
-      $h: 28px;
-      height: $h;
-      line-height: $h;
-      border-radius: $h/2;
-      padding: 0 8px;
+      padding: 8px 24px;
       margin: 0 8px;
+      border-radius: $border-radius;
+
+      &:hover {
+        text-decoration: none;
+      }
     }
   }
 }
