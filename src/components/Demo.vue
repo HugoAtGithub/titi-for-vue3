86823ad5a2cba63cf20d7ab0284f37373e47019d
HugoAtGithub <jorcion@gmail.com>
2020-11-20 18:01:12 +0800

    抽离出 Demo 组件

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,29 +1,37 @@
 <template>
   <div>
-    <h1>Switch 组件示例 </h1>
-    <Demo :component="Switch1Demo"/>
-    <Demo :component="Switch2Demo"/>
+    <div class="demo">
+      <h2>{{ component.__sourceCodeTitle }}</h2>
+      <div class="demo-component">
+        <component :is="component"/>
+      </div>
+      <div class="demo-actions">
+        <Button>查看代码</Button>
+      </div>
+      <div class="demo-code">
+        <pre class="language-html" v-html="Prism.highlight(component.__sourceCode, Prism.languages.html, 'html')"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Switch1Demo from './Switch1.demo.vue';
-import Switch2Demo from './Switch2.demo.vue';
-import Demo from "./Demo.vue";
-import {ref} from "vue";
+import Button from '../lib/Button.vue';
 import Prism from 'prismjs';
 import 'prismjs/themes/prism-solarizedlight.css'
 
 export default {
-  components: {Demo},
+  components: {Button},
+  props: {
+    component: Object
+  },
   setup() {
-    const bool = ref(false)
-    return {bool, Switch1Demo, Switch2Demo, Prism}
+    return {Prism}
   }
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 $border-color: #d9d9d9;
 .demo {
   border: 1px solid $border-color;
@@ -55,6 +63,5 @@ $border-color: #d9d9d9;
     }
   }
 }
-
 </style>
 
