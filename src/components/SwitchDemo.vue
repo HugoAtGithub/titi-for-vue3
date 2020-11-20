b976b555f444d82ef07088899b38d9c340213ab7
HugoAtGithub <jorcion@gmail.com>
2020-11-20 16:21:21 +0800

    抽离用户需要拷贝的代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -4,7 +4,7 @@
     <div class="demo">
       <h2>常规用法</h2>
       <div class="demo-component">
-        <Switch v-model:value="bool"/>
+        <Switch1Demo/>
       </div>
       <div class="demo-actions">
         <Button>查看代码</Button>
@@ -16,7 +16,7 @@
     <div class="demo">
       <h2>支持 disabled </h2>
       <div class="demo-component">
-        <Switch v-model:value="bool" disabled/>
+        <Switch2Demo/>
       </div>
       <div class="demo-actions">
         <Button>查看代码</Button>
@@ -29,12 +29,13 @@
 </template>
 
 <script>
-import Switch from '../lib/Switch.vue';
+import Switch1Demo from './Switch1.demo.vue';
+import Switch2Demo from './Switch2.demo.vue';
 import Button from '../lib/Button.vue';
 import {ref} from "vue";
 
 export default {
-  components: {Switch, Button},
+  components: {Switch1Demo, Switch2Demo, Button},
   setup() {
     const bool = ref(false)
     return {bool}
@@ -73,7 +74,6 @@ $border-color: #d9d9d9;
       margin: 0;
     }
   }
-
 }
 
 </style>
