ea24400e07deda216514a6fa61caf4e0e1d8e3df
HugoAtGithub <jorcion@gmail.com>
2020-11-20 14:27:31 +0800

    支持markdown文件

---
+++ plugins/md.ts
@@ -0,0 +1,30 @@
+import marked from 'marked'
+import * as path from "path";
+import * as fs from "fs";
+
+const mdToJs = str => {
+    const content = JSON.stringify(marked(str))
+    return `export default ${content}`
+}
+
+export function md() {
+    return {
+        configureServer: [
+            async ({app}) => {
+                app.use(async (ctx, next) => {
+                    if (ctx.path.endsWith('.md')) {
+                        ctx.type = 'js';
+                        const filePath = path.join(process.cwd(), ctx.path);
+                        ctx.body = mdToJs(fs.readFileSync(filePath).toString());
+                    } else {
+                        await next()
+                    }
+                })
+            }
+        ],
+        transforms: [{
+            test: context => context.path.endsWith('.md'),
+            transform: ({code}) => mdToJs(code)
+        }]
+    }
+}
